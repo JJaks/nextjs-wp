81120e6063fb08fccc38b0d2e91e5e73186d1016
JJaks <DirolGaming@gmail.com>
2020-12-28 13:38:15 +0200

    Fix typo

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         
-        <p>Hi, I'm <b>Jaagop</b>! I'm a novice web designer and develeoper. I love simplicity and minimalism.</p>
+        <p>Hi, I'm <b>Jaagop</b>! I'm a novice web designer and developer. I love simplicity and minimalism.</p>
         
           <div className="grid">
           <Link href="/me/portfolio"><a className="card">
