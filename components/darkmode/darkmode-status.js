c514b48ff7f0bb7a53247388da3c643b91edfcd5
JJaks <jaagopj@gmail.com>
2022-06-23 20:00:57 +0300

    BFU-06: Removed unused file

--- components/darkmode/darkmode-status.js
+++
@@ -1,9 +0,0 @@
-import useDarkMode from '@fisch0920/use-dark-mode';
-
-const DarkModeStatus = () => {
-    const { value } = useDarkMode(false);
-
-    return value ? 'Dark Mode' : 'Light Mode';
-};
-
-export default DarkModeStatus;
